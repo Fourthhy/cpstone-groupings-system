6a48367e717da3c3656057e3e22a54d3ce36defc
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-01-13 12:11:19 +0000

    configured router

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,14 @@
-const SelectName = () => {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import UserLogin from "./components/user/UserLogin"
+
+export default function App() {
   return (
     <>
-      <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-      <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-        <option selected>Choose a country</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-        <option value="US">United States</option>
-        <option value="CA">Canada</option>
-        <option value="FR">France</option>
-        <option value="DE">Germany</option>
-      </select>
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserLogin />} />
+        </Routes>
+      </Router>
     </>
   )
 }
-
-function App() {
-
-  return (
-    <>
-      <div className="h-screen w-screen flex items-center justify-center bg-graybg">
-        <div className="h-[470px] w-[450px] font-raleway flex justify-center items-center flex-col border-[1px] rounded-lg bg-white">
-          <h2>Capstone Grouping</h2>
-          <form class="max-w-sm mx-auto">
-            <div class="my-5">
-              <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Room Code</label>
-              <input type="text" id="base-input" className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
-            <SelectName />
-            <div className="mt-[15px]">
-              <button type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Room History</button>
-              <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Join Room</button>
-            </div>
-          </form>
-        </div>
-      </div>
-
-    </>
-  )
-}
-
-export default App
