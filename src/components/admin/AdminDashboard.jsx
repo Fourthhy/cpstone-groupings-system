eb6336c90cbf41f942447a378f57be1457478aa9
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-01-14 07:21:13 +0000

    fixed issues

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -66,10 +66,10 @@ const SelectRole = () => {
         <>
             <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
                 <li class="me-2">
-                    <a href="#" aria-current="page" class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Role1</a>
+                    <a href="#" aria-current="page" class="text-blue-600 inline-block p-4 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Role1</a>
                 </li>
                 <li class="me-2">
-                    <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Role2</a>
+                    <a href="#" aria-current="page" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Role2</a>
                 </li>
                 <li class="me-2">
                     <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Role3</a>
@@ -78,7 +78,6 @@ const SelectRole = () => {
                     <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Drop</a>
                 </li>
             </ul>
-
         </>
     )
 }
