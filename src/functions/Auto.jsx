5051a82e621002fa5626b4cfae38763e68841995
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-01-22 00:53:26 +0000

    commited changes

--- src/functions/Auto.jsx
+++ src/functions/Auto.jsx
@@ -1,14 +1,14 @@
 import studentList from "../json/studentList.json"
-import { useAutomation } from "./useAutomation";
+import { useAutomation } from "./backendFunction";
 export default function Auto() {
 
   return (
     <div>
-      <button onClick={useAutomation} style={{border: 'black', backgroundColor: 'white'}}>Click to Create Collection</button>
+      <button onClick={useAutomation} className="border-black border-[1px]">Click to Create Collection</button>
       {studentList.map((name) => (
         <div style={{display: 'flex'}}>
             <p> {atob(name.studentName)} </p>
-            <p> &nbsp; Random User Code: User-{Math.floor(Math.random() * 1000)} </p>
+            <p> &nbs p; Random User Code: User-{Math.floor(Math.random() * 1000)} </p>
         </div>
       ))}
     </div>
