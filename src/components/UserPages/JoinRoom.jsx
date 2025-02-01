b622ea6260ad91e9c8ce35a3c35e9b16d3da6e27
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-02-01 02:07:10 +0000

    commit

--- src/components/UserPages/JoinRoom.jsx
+++ src/components/UserPages/JoinRoom.jsx
@@ -1,7 +1,8 @@
-import { TextInput, Button } from "flowbite-react"
+import { TextInput, Button, Label } from "flowbite-react"
 import { Link } from "react-router-dom"
 
 export default function JoinRoom() {
+
     return (
         <>
             <div className="h-screen w-screen flex items-center justify-center bg-gray-100 text-gray-700">
@@ -9,8 +10,12 @@ export default function JoinRoom() {
                     <div className="w-[380px] h-full flex flex-col justify-center items-center">
                         <h2 className="font-bold text-center text-2xl">Enter Room Code</h2>
                         <div className="mt-[20px] flex max-w-md flex-col gap-2">
-                            <TextInput type="text" placeholder="e.g. 123456" required />
-                            <Link to="/loading"><Button className="w-full" outline gradientDuoTone="cyanToBlue" type="submit" >Submit</Button> </Link>
+
+                            <Label htmlFor="input-gray" color="gray" value="Room Code" />
+                            <TextInput type="text" placeholder="e.g. 123456" required color="gray" />
+                            {/*color="gray" for failed*/}
+
+                            <Link to="/loading"><Button className="w-full mt-[5px]" outline gradientDuoTone="cyanToBlue" type="submit" >Submit</Button> </Link>
                             <div className="flex justify-center items-center gap-[10px]">
                                 <hr className="w-[65px] border-gray-300"/>
                                 <p className="text-gray-500">or</p>
