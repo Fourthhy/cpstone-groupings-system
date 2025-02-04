9d0e9b1d906ba2043087fccf883233dff4e01220
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-02-04 10:07:39 +0000

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import Auto2 from './functions/Auto2.jsx'
 import Auto3 from './functions/Auto3.jsx'
 import AppRoute from './Routes/AppRoute.jsx'
 
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Auto /> */}
