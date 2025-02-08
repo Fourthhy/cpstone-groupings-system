9a487f27ebaf5f772f6b451a089032858f24bfc0
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-02-08 04:16:59 +0000

    UIUX: Error Handling:

--- src/components/UserPages/VouchProcess/JoinRoom.jsx
+++ src/components/UserPages/VouchProcess/JoinRoom.jsx
@@ -6,6 +6,50 @@ export default function JoinRoom() {
 
     const [isLoading, setIsLoading] = useState(false)
 
+    const [inputResponse, setInputResponse] = useState(0)
+    // 0 for neutral
+    // 1 for success
+    // 2 for failure
+
+    const [invalidReason, setInvalidReason] = useState(0)
+
+    // 0 for invalid room code
+    // 1 for room not open
+
+    const handleResponse = () => {
+        inputResponse == 1 ? setInputResponse(1) : inputResponse == 2 ? setInputResponse(2) : setInputResponse(0)
+        if (inputResponse == 2) {
+            //This will set the reason why the room is closed, for example, the set reason is 0 (invalid room code)
+            setInvalidReason(0)
+        }
+    }
+
+    const HelperTextSucess = () => {
+        return (
+            <>
+                <span>Logging in..</span>
+            </>
+        )
+
+    }
+
+    const HelperTextInvalidCode = () => {
+        return (
+            <>
+                <span>Invalid Room Code</span>
+            </>
+        )
+    }
+
+    const HelperTextVouchDisabled = () => {
+        return (
+            <>
+                <span>Room is Closed</span>
+            </>
+        )
+    }
+
+
     const customButtonDesign = "bg-gradient-to-br from-purple-200 to-cyan-200 text-white focus:ring-2 focus:ring-cyan-100 enabled:hover:bg-gradient-to-bl dark:focus:ring-cyan-800 w-full mt-[5px]";
 
     const JoinRoomForm = () => {
@@ -15,7 +59,17 @@ export default function JoinRoom() {
                 <div className="mt-[20px] flex max-w-md flex-col gap-2">
 
                     <Label htmlFor="input-gray" color="gray" value="Room Code" />
-                    <TextInput type="text" placeholder="e.g. 123456" required color="gray" />
+                    <TextInput
+                        type="text"
+                        placeholder="e.g. 123456"
+                        required
+                        color={inputResponse == 1 ? "success" : inputResponse == 2 ? "failure" : "gray"}
+                        helperText={ inputResponse == 1 ? <HelperTextSucess /> : 
+                                     inputResponse == 2 ? 
+                                     invalidReason == 0 ? <HelperTextInvalidCode /> : 
+                                     invalidReason == 1 ? <HelperTextVouchDisabled /> : '' : ''
+                        }
+                    />
                     {/*color="gray" for failed*/}
 
                     <Button
@@ -25,7 +79,7 @@ export default function JoinRoom() {
                         }}
                         outline
                         className={customButtonDesign}
-                        >
+                    >
                         Submit
                     </Button>
 
